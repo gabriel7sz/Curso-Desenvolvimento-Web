e314575ac07dd81d17f2171d3cc6fc8c9891fe88
gabriel7sz <gabriel7sz@outlook.com>
2024-11-19 22:26:20 -0300

    Update
    Organização

---
+++ Section-8-JavaScript/Nota-1-Operadores.js
@@ -0,0 +1,20 @@
+/*
+
+Operadores de comparação :
+
+( == ) igual
+( === ) igual e do mesmo tipo
+( != ) diferentes
+( !== ) diferentes e de tipos diferentes
+( < ) menor
+( > ) maior
+( <= ) menor e igual
+( >= ) maior e igual
+
+Operadores lógicos : 
+
+( && ) E - Verdadeiro se todas as expressões forem verdadeiras
+( || ) OU - Verdadeiro se pelo menos uma das expressões for verdadeira
+( ! ) Negação - Inverte o resultado da expressão de comparação
+
+*/
---
+++ Section-8-JavaScript/Nota-2-OperadoresMatematicos.js
@@ -0,0 +1,13 @@
+/*
+
+    Operadores Aritméticos:
+
+    ( + )  Adição - Soma de valores
+    ( - )  Subatração - Diferença de valores
+    ( * )  Multiplicação - Produto de valores
+    ( / )  Divisão - Quociente dos valores
+    ( % )  Módulo -  Resto Existente em uma operação de divisão
+    ( ++ ) Incremento - Pré/Pós incremento
+    ( -- ) Decremento - Pré/Pós decremento
+
+*/ 
