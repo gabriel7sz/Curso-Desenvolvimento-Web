2fd6123948e3954747af18b78d530a32395395d9
gabriel7sz <gabriel7sz@outlook.com>
2024-11-21 16:12:26 -0300

    Update script.js

--- Section-9-ECMAScipt-OPP/aula024-PrototypeHerança/script.js
+++ Section-9-ECMAScipt-OPP/aula024-PrototypeHerança/script.js
@@ -10,5 +10,15 @@
  * ou objetos literáis, temos condições de referenciar qual que é o protótipo do objeto
  * que está sendo criado para estabelecer a relação de HERANÇA.
  * 
- * Como qualquer um dos métodos que utilizamos em JavaScript
  * */ 
+
+
+// Neste momento os 3 objetos apontam para o protype de Object. Então na essência todos são filhos de Object.prototype
+let animal = {  attr1: 'a'};
+let vertebrado = {  attr2: 'b'};
+let ave = { attr3: 'c'};
+
+//Os 3 objetos serem filhos significa, que este console.log retornará undefined. Pois o attr2 é um atributo existente em vertebrado. Isso quer dizer que não existe uma relação de herança estabelecido entre ave e vertebrado, mas sim, entre ave e Object.prototype. Ou seja, ave ainda não aponta para o prototype de invertebrado.
+
+console.log(ave.attr2);
+// Porém se ajustar a relação de protótipo, fazendo com que, ao invés por padrão o protótipo aponte para Object.prototype onde o atributo não existe, passando a apontar para 'vertebrado' e assim 'ave' passa a herdar os atributos de vertebrado.
